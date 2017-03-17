264ccc7f1afa05597fdcc839a080c64457d0a5f5
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-03-17 09:23:57 -0700

    separate front end from back end logic with displayDoctors method moving to interface file

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,6 +1,10 @@
 var apiKey = require('./../.env').apiKey;
 var Doctor = require('./../js/doctor.js').doctorModule;
 
+var displayDoctors = function(medicalIssue, doctorFirstName, doctorLastName) {
+$('#showDoctors').text("The doctors that matched your request for treatment of " + medicalIssue + " are: " + response.profile.first_name + " " + response.profile.last_name);
+  console.log(response);
+}
 
 $(document).ready(function() {
   var newDoctor = new Doctor();
@@ -9,4 +13,5 @@ $(document).ready(function() {
     $('#med-issue').val("");
     newDoctor.getDoctors(medicalIssue);
   });
+
 });
