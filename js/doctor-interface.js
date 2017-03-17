ef40355adf09b66505c6723abc5451bc4dcabdec
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-03-17 10:56:05 -0700

    fix it so that api runs but does not yet display any results

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -11,20 +11,17 @@ $(document).ready(function() {
   $('#getDoctor').click(function(){
     console.log("click running");
     var medicalIssue = $('#med-issue').val();
+    console.log(medicalIssue);
     $('#med-issue').val("");
-    exports.getDoctors = function(medicalIssue) {
-      $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+      console.log("get doctors method starts");
+      $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + "2f3e04b24230ea345871383997e30c90")
        .then(function(response) {
         //  other actions go here
         console.log("function running");
-        displayDoctors(medicalIssue, response.profile.last_name);
+        $('#showDoctors').text("The doctors that matched your request for treatment of ");
         })
        .fail(function(error){
           console.log("fail");
         });
-    };
-
-    $('#showDoctors').text("test");
+    });
   });
-
-});
