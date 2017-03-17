819f52d08b01d1cfa0d95d1e0ec8ec429a60087d
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-03-17 15:05:17 -0700

    make page fully functional but not refactored for front and back end separation

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -3,28 +3,23 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 
 var doctorsArray = [];
 
-var displayDoctors = function(medicalIssue, doctorLastName) {
-  $('#showDoctors').text("The doctors that matched your request for treatment of ");
-  console.log(response);
+var displayDoctors = function(response, medicalIssue) {
+  $(".results").show();
+  $("#med-concern").text(medicalIssue);
+  for(var i=0; i<response.data.length; i++){
+    $('#showDoctors').append("<li><span class='docName'>Dr. " +  response.data[i].profile.first_name + " " + response.data[i].profile.last_name + ",</span> " + response.data[i].profile.title + " at " + response.data[i].practices[0].name + "</li>");
+  };
 }
 
 $(document).ready(function() {
   // var newDoctor = new Doctor();
   $('#getDoctor').click(function(){
     var medicalIssue = $('#med-issue').val();
+    console.log(medicalIssue);
     $('#med-issue').val("");
-      $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + "2f3e04b24230ea345871383997e30c90")
-       .then(function(response) {
-        //  other actions go here
-        doctorsArray.push(response);
-        console.log(doctorsArray);
-        console.log(doctorsArray[0].data[0].profile.last_name);
-        for(var i=0; i<response.data[0].length; i++){
-          $('#showDoctors').text("The doctor that matched your request for treatment of " + medicalIssue + " is: Dr. " +  response.data[1].profile.last_name);
-        };
-      });
-      //  .fail(function(error){
-      //     console.log("fail");
-      //   });
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + "f1abe4d78dbdd5e9d675b6d0de0b2c1d")
+    .then(function(response) {
+      displayDoctors(response, medicalIssue);
     });
   });
+});
--- js/doctor.js
+++ js/doctor.js
@@ -4,13 +4,9 @@ function Doctor(){
 }
 
 Doctor.prototype.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(response) {
-    //  other actions go here
-    console.log("function running");
-    displayDoctors(medicalIssue, response.profile.last_name);
-    })
-   .fail(function(error){
-      console.log("fail");
-    });
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + "f1abe4d78dbdd5e9d675b6d0de0b2c1d")
+  .then(function(response) {
+    displayDoctors(response, medicalIssue);
+  });
 };
+exports.doctorModule = Doctor;
