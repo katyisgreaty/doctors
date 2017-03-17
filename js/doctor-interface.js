853274742f3ca6ad0d5a45e26608364b2fdf0853
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-03-17 10:46:29 -0700

    move javascript to front end, now click function runs though API does not

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -7,16 +7,24 @@ var displayDoctors = function(medicalIssue, doctorLastName) {
 }
 
 $(document).ready(function() {
-  var newDoctor = new Doctor();
-  $('#getDoctor').submit(function(event){
-    event.preventDefault();
+  // var newDoctor = new Doctor();
+  $('#getDoctor').click(function(){
     console.log("click running");
-    // var medicalIssue = $('#med-issue').val();
-    // $('#med-issue').val("");
-    // newDoctor.getDoctors(medicalIssue);
+    var medicalIssue = $('#med-issue').val();
+    $('#med-issue').val("");
+    exports.getDoctors = function(medicalIssue) {
+      $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+       .then(function(response) {
+        //  other actions go here
+        console.log("function running");
+        displayDoctors(medicalIssue, response.profile.last_name);
+        })
+       .fail(function(error){
+          console.log("fail");
+        });
+    };
+
     $('#showDoctors').text("test");
   });
 
 });
-
-// + medicalIssue + " are: " + response.profile.first_name + " " + response.profile.last_name
